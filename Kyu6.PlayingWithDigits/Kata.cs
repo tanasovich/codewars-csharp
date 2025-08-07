5ed9802fcbd476c90a3bf0add4ae454d4f081fdd
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2025-08-07 16:59:54 +0200

    Solution

--- Kyu6.PlayingWithDigits/Kata.cs
+++ Kyu6.PlayingWithDigits/Kata.cs
@@ -4,6 +4,25 @@ public class Kata
 {
     public static long DigitsPower(int number, int firstPower)
     {
-        return -1;
+        int currentNumber = number;
+        int lastPower = firstPower + CountNumberOfDigits(number) - 1;
+        long powerOfDigitsSum = 0;
+
+        for (int i = lastPower; i >= firstPower; i--)
+        {
+            int digit = currentNumber % 10;
+            currentNumber /= 10;
+
+            powerOfDigitsSum += (long) Math.Pow(digit, i);
+        }
+
+        long quotient = powerOfDigitsSum / number;
+
+        return (powerOfDigitsSum % number == 0)? quotient : -1;
+    }
+
+    public static int CountNumberOfDigits(int number)
+    {
+        return (int) Math.Floor(Math.Log10(number)) + 1;
     }
 }
