098ac1f8c0d0bb24e6c699fd7a37bee7d1bab8eb
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2025-08-07 15:59:06 +0200

    Added second boundary value test
    Boundary value tests have appropriate labels.

--- Kyu8.HowGoodAreYouReally.Tests/Tests.cs
+++ Kyu8.HowGoodAreYouReally.Tests/Tests.cs
@@ -31,11 +31,27 @@ public class Tests
     }
 
     [Test]
+    [Category("Boundary Value")]
     public void BetterThanAverageWithEqualAverageReturnsFalse()
     {
         //Arrange
         int[] grades = [3, 3, 3, 3, 3];
         int myGrade = 3;
+
+        //Act
+        bool actual = Kata.BetterThanAverage(grades, myGrade);
+
+        //Assert
+        Assert.That(actual, Is.False);
+    }
+
+    [Test]
+    [Category("Boundary Value")]
+    public void BetterThanAverageWithSubtlyHigherAverageReturnsFalse()
+    {
+        //Arrange
+        int[] grades = [1, 5, 10, 15, 20];  // Average equals to 10.1
+        int myGrade = 10;
 
         //Act
         bool actual = Kata.BetterThanAverage(grades, myGrade);
