812f5a65ef3d60c3fccbe226c1a6acbbb742b14c
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2025-08-09 12:40:20 +0200

    Solution

--- Kyu8.TheFallingSpeedOfPetals/Kata.cs
+++ Kyu8.TheFallingSpeedOfPetals/Kata.cs
@@ -2,8 +2,23 @@
 
 public class Kata
 {
+    /// <summary>
+    /// To fall down from branch <i>x<i/>, petal with velocity 5 cm/s takes
+    /// 80 seconds. So, branch is at the 5 * 80 = 400 cm height above
+    /// the ground.
+    /// </summary>
+    /// <remarks>
+    /// Height is in centimeters.
+    /// </remarks>
+    private static readonly int BRANCH_HEIGHT = 400;
+
     public static double SakuraFall(double petalVelocity)
     {
-        return 0;
+        if (petalVelocity <= 0)
+        {
+            return 0;
+        }
+        
+        return BRANCH_HEIGHT / petalVelocity;
     }
 }
