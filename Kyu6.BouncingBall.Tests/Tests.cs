dbc51496b084d2502b22523dc47ca90d65c069ac
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2025-08-10 14:35:12 +0200

    Boundary value tests

--- Kyu6.BouncingBall.Tests/Tests.cs
+++ Kyu6.BouncingBall.Tests/Tests.cs
@@ -20,7 +20,6 @@ public class Tests
 
     [Test]
     [Category("Positive")]
-    [Category("Boundary Value")]
     [Description("Ball is seen exactly one time")]
     public void BallIsSeenCountWithBottomBoundaryReturnsOne()
     {
@@ -38,7 +37,6 @@ public class Tests
 
     [Test]
     [Category("Positive")]
-    [Category("Boundary Value")]
     [Description("Ball is seen exactly three times")]
     public void BallIsSeenCountWithTopBoundaryReturnsThree()
     {
@@ -52,5 +50,17 @@ public class Tests
 
         //Assert
         Assert.That(actual, Is.EqualTo(3));
+    }
+
+    [Test]
+    [TestCase(1e-5, 0.5, 1e-6, ExpectedResult = 7, Description = "Height positive edge")]
+    [TestCase(0, 0.5, 21, ExpectedResult = -1, Description = "Height negative edge")]
+    [TestCase(42, 1e-6, 21, ExpectedResult = 1, Description = "Bounce positive low edge")]
+    [TestCase(42, 0, 21, ExpectedResult = -1, Description = "Bounce negative low edge")]
+    [TestCase(42, 999999e-6, 21, ExpectedResult = 1386293, Description = "Bounce positive high edge")]
+    [TestCase(42, 1, 21, ExpectedResult = -1, Description = "Bounce negative high edge")]
+    public int BallIsSeenCountWithInvalidDataReturnsMinusOne(double height, double bounceRate, double windowHeight)
+    {
+        return Kata.BallIsSeenCount(height, bounceRate, windowHeight);
     }
 }
