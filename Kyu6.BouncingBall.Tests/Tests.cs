53a0e1a53e97c76558806756aa6928614eafc5d3
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2025-08-10 13:38:43 +0200

    Empty test project

---
+++ Kyu6.BouncingBall.Tests/Tests.cs
@@ -0,0 +1,15 @@
+namespace Kyu6.BouncingBall.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
