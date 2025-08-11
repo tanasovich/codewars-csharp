9ab7c3033e2e3eba8b90efc38341500dde0981ef
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2025-08-11 23:13:18 +0200

    Added boundary value tests

--- Kyu8.GrasshopperGradeBook.Tests/Tests.cs
+++ Kyu8.GrasshopperGradeBook.Tests/Tests.cs
@@ -2,14 +2,20 @@ namespace Kyu8.GrasshopperGradeBook.Tests;
 
 public class Tests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
-
     [Test]
-    public void Test1()
+    [Category("Boundary Value")]
+    [TestCase(0, 0, 0, ExpectedResult = 'F')]
+    [TestCase(59, 59, 59, ExpectedResult = 'F')]
+    [TestCase(60, 60, 60, ExpectedResult = 'D')]
+    [TestCase(69, 69, 69, ExpectedResult = 'D')]
+    [TestCase(70, 70, 70, ExpectedResult = 'C')]
+    [TestCase(79, 79, 79, ExpectedResult = 'C')]
+    [TestCase(80, 80, 80, ExpectedResult = 'B')]
+    [TestCase(89, 89, 89, ExpectedResult = 'B')]
+    [TestCase(90, 90, 90, ExpectedResult = 'A')]
+    [TestCase(100, 100, 100, ExpectedResult = 'A')]
+    public char GetGradeWithBoundaryValuesReturnsCorrectGrade(int first, int second, int third)
     {
-        Assert.Pass();
+        return Kata.GetGrade(first, second, third);
     }
 }
