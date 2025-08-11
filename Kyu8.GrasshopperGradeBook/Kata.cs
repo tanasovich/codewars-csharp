525593c221f388e7edeac029a56bda208945f567
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2025-08-11 22:59:07 +0200

    Solution

--- Kyu8.GrasshopperGradeBook/Kata.cs
+++ Kyu8.GrasshopperGradeBook/Kata.cs
@@ -2,5 +2,26 @@
 
 public class Kata
 {
+    public static char GetGrade(int firstGrade, int secondGrade, int thirdGrade)
+    {
+        double average = (firstGrade + secondGrade + thirdGrade) / 3.0;
+        if (average < 60)
+        {
+            return 'F';
+        }
+        else if (average < 70)
+        {
+            return 'D';
+        }
+        else if (average < 80)
+        {
+            return 'C';
+        }
+        else if (average < 90)
+        {
+            return 'B';
+        }
 
+        return 'A';
+    }
 }
