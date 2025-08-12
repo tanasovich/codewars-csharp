c6ee0e5a543bd19697a0dc4e26f65d699fc642ab
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2025-08-12 13:28:18 +0200

    One person likes check

--- Kyu6.WhoLikesIt.Tests/Tests.cs
+++ Kyu6.WhoLikesIt.Tests/Tests.cs
@@ -3,6 +3,20 @@ namespace Kyu6.WhoLikesIt.Tests;
 public class Tests
 {
     [Test]
+    public void LikesWitOneNameReturnsCorrectLikes()
+    {
+        //Arrange
+        string[] names = new string[0];
+        string expected = "no one likes this";
+
+        //Act
+        string actual = Kata.Likes(names);
+
+        //Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
     public void LikesWithoutNamesReturnsNobodyLikes()
     {
         //Arrange
