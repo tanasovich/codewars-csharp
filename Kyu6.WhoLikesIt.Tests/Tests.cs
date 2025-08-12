fd5e3330b60eb1b0e6ee5263be89253876a79e4b
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2025-08-12 13:06:22 +0200

    Empty test project

---
+++ Kyu6.WhoLikesIt.Tests/Tests.cs
@@ -0,0 +1,15 @@
+namespace Kyu6.WhoLikesIt.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
