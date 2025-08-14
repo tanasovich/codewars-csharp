d4e8d8e77c3ed95a318e68968683c36ab1907e78
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2025-08-14 13:02:10 +0200

    Solution

--- Kyu8.BodyMassIndex/Kata.cs
+++ Kyu8.BodyMassIndex/Kata.cs
@@ -4,6 +4,20 @@ public class Kata
 {
     public static string Bmi(double weight, double height)
     {
-        throw new NotImplementedException();
+        double bmi = weight / Math.Pow(height, 2);
+        if (bmi <= 18.5)
+        {
+        return "Underweight";
+        }
+        else if (bmi <= 25.0)
+        {
+        return "Normal";
+        }
+        else if (bmi <= 30.0)
+        {
+        return "Overweight";
+        }
+        
+        return "Obese";
     }
 }
