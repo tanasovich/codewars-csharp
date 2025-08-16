822a665d16095db0522279e20d1f488cb48a6349
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2025-08-16 14:56:00 +0200

    Solution

--- Kyu6.ReplaceWithAlphabetIndex/Kata.cs
+++ Kyu6.ReplaceWithAlphabetIndex/Kata.cs
@@ -2,5 +2,21 @@
 
 public class Kata
 {
+    public static string AlphabetPosition(string text)
+    {
+        return String.Join(" ", text.Aggregate(new List<byte>(), (codes, x) =>
+        {
+            if (!Char.IsLetter(x))
+            {
+                return codes;
+            }
+            codes.Add(GetAlphabeticalPosition(x));
+            return codes;
+        }));
+    }
 
+    private static byte GetAlphabeticalPosition(char character)
+    {
+        return (byte)(Char.ToLower(character) - 'a' + 1);
+    }
 }
