2ad50afd5577d338d63d618bc362b84cf8868926
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2025-08-16 15:14:07 +0200

    Check non-letters are ignored

--- Kyu6.ReplaceWithAlphabetIndex.Tests/Tests.cs
+++ Kyu6.ReplaceWithAlphabetIndex.Tests/Tests.cs
@@ -16,4 +16,20 @@ public class Tests
         // Assert
         Assert.That(actual, Is.EqualTo(expected));
     }
+
+    [Category("Positive")]
+    [TestCase("\tHello World", Description = "Ignores white spaces")]
+    [TestCase("Hel2lo0World5", Description = "Ignores digits")]
+    [TestCase("@HelloW#$orld", Description = "Ignores special chars")]
+    public void AlphabetPositionWithMixedTextIgnoresNonLetters(string source)
+    {
+        // Arrange
+        string expected = "8 5 12 12 15 23 15 18 12 4";
+
+        // Act
+        string actual = Kata.AlphabetPosition(source);
+
+        // Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
 }
